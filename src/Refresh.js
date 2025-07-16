6be18b21191a353da184ed7c37c054a60bdc6739
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2025-07-16 11:52:35 -0400

    Update Refresh.js

--- src/Refresh.js
+++ src/Refresh.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useParams} from "react-router";
+import { useParams } from "react-router";
 
 export default function Refresh() {
   const {connectedAccountId} = useParams();
