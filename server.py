6f4be5b66988b5646d5f13f4aaa80c2c13b15954
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2025-07-16 17:50:18 -0400

    Update server.py

--- server.py
+++ server.py
@@ -60,4 +60,4 @@ def catch_all(path, **kwargs):
         return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', debug=false, port=8080)
