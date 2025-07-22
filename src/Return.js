68b417955f8b46412e1b529db5c3d8cdae124a7d
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2025-07-22 12:42:18 -0400

    Update Return.js

--- src/Return.js
+++ src/Return.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-export default function Return() {
+export function Return() {
   const {connectedAccountId} = useParams();
 
   return (
