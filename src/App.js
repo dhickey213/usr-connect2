cbcdb68ddbad5d8139700b42af6df049df1847c6
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2025-07-22 12:16:09 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./Home";
-import Refresh from "/.Refresh";
-import Return from "/.Return";
+import Refresh from "./Refresh";
+import Return from "./Return";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
