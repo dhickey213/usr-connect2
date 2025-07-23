cd4137febb690625abcd6e98827aeec9a1d37abd
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2025-07-22 21:44:02 -0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,1 +1,7 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+import App from "./App";
 
+<h2>Get ready for take off</h2>
+const root = createRoot(document.getElementById("root"))
+root.render(<App />);
