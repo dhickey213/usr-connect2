e2a087a3a69176a5a5137a77dbcdfae02712b06a
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2025-07-22 22:33:20 -0400

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import {createRoot} from "react-dom/client";
-import App from "./App";
-
-<h2>Get ready for take off</h2>
-const root = createRoot(document.getElementById("root"))
-root.render(<App />);
