e410aa4c092d37b8883ef888be5279b830a0e54f
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2025-07-22 21:48:11 -0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-import App from "./App";
+import App from "..src/App";
 
 <h2>Get ready for take off</h2>
 const root = createRoot(document.getElementById("root"))
