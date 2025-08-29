15ec51eb2200643f1cd2501ee706a9418900fb58
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2025-08-29 11:26:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -74,22 +74,23 @@ def createCharge():
     unit_amount = int(data['unit_amount'])
     acctID = data['acctID']
     try:
-      session = stripe.checkout.Session.create(
-      line_items=[
-        {
+        session = stripe.checkout.Session.create(
+    line_items=[
+      {
           "price_data": {
-            "currency": "usd",
-            "product_data": {"name": appID},
-            "unit_amount": (unit_amount * 100),
-          },
-          "quantity": 1,
+          "currency": "usd",
+          "product_data": {"name": appID},
+          "unit_amount": unit_amount,
         },
-      ],
-      #payment_intent_data={"application_fee_amount": 1},
-      mode="payment",
-      success_url=f"https://sample-react-rt2za.ondigitalocean.app/success?session_id={CHECKOUT_SESSION_ID}",
-      stripe_account='{{CONNECTED_ACCOUNT_ID}}',
-      )
+        "quantity": 1,
+      },
+    ],
+    #payment_intent_data={"application_fee_amount": 123},
+    mode="payment",
+    success_url="https://example.com/success?session_id={CHECKOUT_SESSION_ID}",
+    #stripe_account='{{CONNECTED_ACCOUNT_ID}}',
+    stripe_account=acctID,
+    )
       return jsonify ({'url':success_url})
     
     except Exception as e:
