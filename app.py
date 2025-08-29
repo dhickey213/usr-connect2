f38dda032fafaa5b591268801d4a3bdce53d36e0
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2025-08-29 12:31:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -73,25 +73,25 @@ def createCharge():
     appID = data['appID']
     unit_amount = int(data['unit_amount'])
     acctID = data['acctID']
-  try:
-    session = stripe.checkout.Session.create(  
-         line_items=[
-            {
-                "price_data": {
-                 "currency": "usd",
-                "product_data": {"name": appID},
-                "unit_amount": unit_amount,
-              },
-              "quantity": 1,
-            },
-          ],
-          #payment_intent_data={"application_fee_amount": 123},
-           mode="payment",
-           success_url="https://example.com/success?session_id={CHECKOUT_SESSION_ID}",
-           stripe_account=acctID,
-       )
-    return jsonify({'url':success_url})
+    try:
+        session = stripe.checkout.Session.create(  
+             line_items=[
+                {
+                    "price_data": {
+                     "currency": "usd",
+                    "product_data": {"name": appID},
+                    "unit_amount": unit_amount,
+                  },
+                  "quantity": 1,
+                },
+              ],
+              #payment_intent_data={"application_fee_amount": 123},
+                mode="payment",
+                success_url="https://example.com/success?session_id={CHECKOUT_SESSION_ID}",
+               stripe_account=acctID,
+           )
+        return jsonify({'url':success_url})
            
-  except Exception as e:
+      except Exception as e:
         print('An error occurred when calling the Stripe API to create an account link: ', e)
         return jsonify(error=str(e)), 500
